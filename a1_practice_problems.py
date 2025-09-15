3eb2f07812586844c861c3a3c7d29d45f7bb88c3
Maddox Bartoli <mtbartoli@cps.edu>
2025-09-15 10:25:30 -0500

    finnished absolute, factorial, median, and duck_duck_goose

--- a1_practice_problems.py
+++ a1_practice_problems.py
@@ -27,7 +27,9 @@ def absolute(n: int) -> int:
     Returns:
         the absolute value of the passed in number
     """
-    raise NotImplementedError("absolute")
+    if n < 0:
+        return -1 * n
+    return n
 
 
 def factorial(n: int) -> int:
@@ -40,8 +42,10 @@ def factorial(n: int) -> int:
     Returns:
         factorial of the passed in number
     """
-    raise NotImplementedError("factorial")
-
+    result = 1
+    for num in range(1, n+1):
+        result = result * num  # result *= num
+    return result
 
 T = TypeVar("T")
 
@@ -106,7 +110,7 @@ def median(lst: List[int]) -> float:
     Returns:
         the median of the passed in list
     """
-    raise NotImplementedError("median")
+    return sum_list(lst) / len(lst) if lst else 0
 
 
 def duck_duck_goose(lst: List[str]) -> List[str]:
@@ -128,23 +132,36 @@ def duck_duck_goose(lst: List[str]) -> List[str]:
     Returns:
         the resulting list after playing duck duck goose
     """
-    raise NotImplementedError("duck_duck_goose")
+    if len(lst) % 2 == 1:
+        return lst[len(lst) // 2 ]
+    else:
+        m1 = len(lst) // 2
+        m2 = m1 - 1
+        return (lst[m1] + lst[m2]) / 2
+
 
 
 # this line causes the nested code to be skipped if the file is imported instead of run
 if __name__ == "__main__":
     assert absolute(-1) == 1, "absolute of -1 failed"
+    assert absolute(4) == 4, "absolute of 4 failed"
     assert factorial(4) == 24, "factorial of 4 failed"
-    assert every_other([1, 2, 3, 4, 5]) == [
-        1,
-        3,
-        5,
-    ], "every_other of [1,2,3,4,5] failed"
+    assert factorial(5) == 120, "factorial of 5 failed"
+    assert factorial(1) == 1, "factorial of 1 failed"
+    assert factorial(0) == 1, "factorial of 0 failed"
+    assert every_other([1, 2, 3, 4, 5]) == [1,3,5], "every_other of [1,2,3,4,5] failed"
+    assert every_other([12, 23, 35, 48, 53, 63]) == [12,35,53], "every_other of [1,2,3,4,5] failed"
     assert sum_list([1, 2, 3]) == 6, "sum_list of [1,2,3] failed"
+    assert sum_list([4, 7, 10, 23, 7]) == 51, "sum_list of [4, 7, 10, 23, 7] failed"
+    # print(sum_list([4, 7, 10, 23, 7]))
     assert mean([1, 2, 3, 4, 5]) == 3, "mean of [1,2,3,4,5] failed"
+    assert mean([1, 2, 3, 4, 5, 6]) == 21/6, "mean of [1, 2, 3, 4, 5, 6] failed"
+    assert mean([]) == 0, "mean of [] failed"
     assert median([1, 2, 3, 4, 5]) == 3, "median of [1,2,3,4,5] failed"
-
+    assert median([1, 2, 3, 4, 6, 6]) == 3.5, "median of [1, 2, 3, 4, 5, 6] failed"
     names = ["roscoe", "kim", "woz", "solin", "law", "remess"]
-    assert duck_duck_goose(names) == ["roscoe", "law"]
+    assert duck_duck_goose(names) == ["roscoe", "law"], "duck duck goose assert 1 failed"
+    names = ["avery", "levi", "ashley", "victoria", "maddox"]
+    assert duck_duck_goose(names) == ["levi", "victoria"], "duck duck goose assert 2 failed"
 
     print("All tests passed!")
